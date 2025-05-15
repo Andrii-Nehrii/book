36022f0e7df0949da18c05ee03db846a54f0c994
Andrii-Nehrii <negriyandriy@gmail.com>
2025-05-16 00:07:26 +0200

    3

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const  titleEl = document.getElementById("book-title");
-const authorEl = document.getElementById("book-author");
-const yearEl = document.getElementById("book-year");
+const  titleEl = document.getElementById("title");
+const authorEl = document.getElementById("author");
+const yearEl = document.getElementById("year");
 
 
 fetch("https://andrii-nehrii.github.io/book/book.json").then((res) => {
     if(!res.ok){
         throw new Error("-vibe")
     }
-    return res.json
+    return res.json()
 }).then((data)=>{
     const {title, author,year} = data;
 titleEl.innerText= title;
