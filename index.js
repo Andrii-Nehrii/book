9abd2ede28ab72fd07f1bec71a2cb1ea000e2fd1
Andrii-Nehrii <negriyandriy@gmail.com>
2025-05-20 23:07:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,22 @@
-// // 1. Отправить запрос на https://api.sampleapis.com/wines/whites и получить (вывести в консоль) массив с данными о белых винах
-// 2. Отрисовать карточки вин с рейтингом 4.8 и более (image, wine, winery, rating)
-// 3. Стилизовать карточки (border, padding, border-radius). Из контейнера сделать grid - 5 колонок + отступы
-//  display: grid;
-//  grid-template-columns: repeat(5, 1fr);
-//  gap: 40px;
-// 4. Если у вина рейтинг 4.9 и больше то покрасить карточку в светло-зеленый. А если 4.8 - то в светло-голубой
-const container = document.querySelector(".container");
+const button = document.getElementById("refreshButton");
 
-fetch("https://api.sampleapis.com/wines/whites")
-.then(res => res.json())
-.then(json => render(json))
+const url = "https://wttr.in/";
+const params = {
+    7: "",
+    m: "",
+    lang: "ru",
+    
+   
+};
 
-const render =  array =>{
-array
-.filter(el =>el.rating.average >= 4.8)
-.forEach(({image, wine, winery, rating})=>{
-    const cardElem = document.createElement("div");
-    const titleElem = document.createElement("p");
-    const wineryElem = document.createElement("p");
-    const ratingElem = document.createElement("p");
-    const imgElem = document.createElement("img");
-    titleElem.innerText = `Title: ${wine}`;
-    wineryElem.innerText = `Winery: ${winery}`;
-    ratingElem.innerText = `Rating: ${rating.average}`;
-
-    imgElem.src = image;
-    imgElem.alt = wine;
-    cardElem.style.backgroundColor = rating.average > 4.8 ? "lightgreen" : "lightblue"
-    cardElem.append(imgElem,titleElem,wineryElem,ratingElem);
-    container.append(cardElem);
-})
+function fetchData() {
+    const queryString = new URLSearchParams(params).toString();
+    fetch(`${url}?${queryString}`)
+        .then((response) => response.text())
+        .then((pageText) => {
+            const result = document.getElementById("content");
+            result.innerHTML = pageText;
+        });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const {winery, wine, rating,location ,image} = obj;
-//    const wineImage = image;
-//    const avRating = rating.average;
+button.addEventListener("click", fetchData);
